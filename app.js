1c02e77919fd13caa3da18210b969599b4508b75
Ryan Dowling <rldowling03@gmail.com>
2015-03-27 02:11:26 +1100

    Forgot to update license in rename

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
- * Minecraft Server Checker - https://github.com/RyanTheAllmighty/Minecraft-Server-Checker
+ * Minecraft Server Tools - https://github.com/RyanTheAllmighty/Minecraft-Server-Tools
  * Copyright (C) 2015 RyanTheAllmighty
  *
  * This program is free software: you can redistribute it and/or modify
