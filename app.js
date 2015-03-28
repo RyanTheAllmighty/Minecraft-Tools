ec493938e9ecef2b0eea0028f6946b592e625fcc
Ryan Dowling <rldowling03@gmail.com>
2015-03-28 22:37:34 +1100

    Add in optional logging to Sentry

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ var dns = require('dns');
 var ping = require('mc-ping');
 var votifier = require('votifier-send');
 
-
 var env = require('node-env-file');
 env(__dirname + '/.env');
 
@@ -33,8 +32,22 @@ app.use(bodyParser.json());
 
 var validators = require('./validators');
 
+if (process.env.ENABLE_SENTRY) {
+    var raven = require('raven');
+
+    var client = new raven.Client(process.env.SENTRY_DSN);
+}
+
 app.use(function (req, res, next) {
     if (req.body.auth != process.env.AUTH_KEY) {
+        var err = new Error('Invalid auth provided!');
+
+        if (process.env.ENABLE_SENTRY) {
+            client.captureError(err, {extra: {body: res.body}});
+        } else {
+            console.error(err);
+        }
+
         return res.status(400).send('Invalid auth provided!');
     }
 
@@ -43,20 +56,29 @@ app.use(function (req, res, next) {
 
 app.post('/query', function (req, res) {
     if (!validators.queryValidator(req.body)) {
+        var err = new Error('Invalid JSON provided!');
+
+        if (process.env.ENABLE_SENTRY) {
+            client.captureError(err, {extra: {body: res.body}});
+        } else {
+            console.error(err);
+        }
+
         return res.status(400).send('Invalid JSON provided!');
     }
 
-    console.log('Querying server ' + req.body.host + ":" + req.body.port);
+    if (process.env.ENABLE_SENTRY) {
+        client.captureMessage('Querying server ' + req.body.host + ":" + req.body.port)
+    } else {
+        console.log(data);
+    }
 
     dns.resolveSrv('_minecraft._tcp.' + req.body.host, function (err, data) {
         var originalHost = req.body.host;
         var originalPort = req.body.port;
 
         if (data && data[0] && data[0].port) {
-            console.log('The server has an SRV record for port ' + data[0].port + '!');
-
             if (data[0].name) {
-                console.log('The server also has an SRV record for host ' + data[0].name + '!');
                 req.body.host = data[0].name;
             }
 
@@ -67,7 +89,12 @@ app.post('/query', function (req, res) {
 
         ping(req.body.host, req.body.port, function (err, data) {
             if (err) {
-                console.error(err);
+                if (process.env.ENABLE_SENTRY) {
+                    client.captureError(err)
+                } else {
+                    console.error(err);
+                }
+
                 return res.status(200).send({
                     id: req.body.id,
                     host: originalHost,
@@ -77,7 +104,6 @@ app.post('/query', function (req, res) {
                     reason: err.message
                 });
             } else {
-                console.log(data);
                 return res.status(200).send({
                     id: req.body.id,
                     host: originalHost,
@@ -97,10 +123,14 @@ app.post('/query', function (req, res) {
 
 app.post('/vote', function (req, res) {
     if (!validators.voteValidator(req.body)) {
-        return res.status(400).send('Invalid JSON provided!');
-    }
+        var err = new Error('Invalid JSON provided!');
 
-    console.log('Sending votifier vote for ' + req.body.username + ' to ' + req.body.host + ":" + req.body.port);
+        if (process.env.ENABLE_SENTRY) {
+            client.captureError(err, {extra: {body: res.body}});
+        } else {
+            console.error(err);
+        }
+    }
 
     votifier.send({
         key: req.body.key,
@@ -113,7 +143,11 @@ app.post('/vote', function (req, res) {
         }
     }, function (err) {
         if (err) {
-            console.error(err);
+            if (process.env.ENABLE_SENTRY) {
+                client.captureError(err)
+            } else {
+                console.error(err);
+            }
         }
 
         return res.status(200).send({
